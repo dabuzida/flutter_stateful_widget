d654dd512e33f577b0616762f9f060706a48f56f
dabuzida <dabuzida@gmail.com>
2022-07-20 18:04:40 +0900

    1

---
+++ lib/test.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class Test extends StatelessWidget {
+  const Test({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Test2();
+  }
+}
+
+class Test2 extends StatefulWidget {
+  const Test2({Key? key}) : super(key: key);
+
+  @override
+  State<Test2> createState() => _Test2State();
+}
+
+class _Test2State extends State<Test2> {
+  @override
+  Widget build(BuildContext context) {
+    return Text('d');
+  }
+}
